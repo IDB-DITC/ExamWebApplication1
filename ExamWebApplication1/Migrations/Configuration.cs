6ae69b104542e2f502319f81915c6ed58723d74d
idbditc@gmail.com <idbditc@gmail.com>
2024-01-13 17:25:45 +0600

    fix all 3#

---
+++ ExamWebApplication1/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+﻿namespace ExamWebApplication1.Migrations
+{
+	using System;
+	using System.Data.Entity;
+	using System.Data.Entity.Migrations;
+	using System.Linq;
+
+	internal sealed class Configuration : DbMigrationsConfiguration<ExamWebApplication1.Models.AppDb>
+	{
+		public Configuration()
+		{
+
+			AutomaticMigrationsEnabled = true;
+
+		}
+
+		protected override void Seed(ExamWebApplication1.Models.AppDb context)
+		{
+			//  This method will be called after migrating to the latest version.
+
+			//  You can use the DbSet<T>.AddOrUpdate() helper extension method
+			//  to avoid creating duplicate seed data.
+
+
+			context.Users.AddOrUpdate(new Models.UserInfo() { UserName = "admin", Password = "abc123", Role = "Admin" });
+
+
+			context.Users.AddOrUpdate(new Models.UserInfo() { UserName = "ditc", Password = "123456", Role = "User" });
+
+		}
+	}
+}
